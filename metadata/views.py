baa77b385004ae4a6716aaae591254dfa29dfb6b
Joel Onojason <joel.onojason@outlook.com>
2022-05-08 19:56:20 +0100

    update to get music method

--- metadata/views.py
+++ metadata/views.py
@@ -10,6 +10,10 @@ class MusicViewSet(viewsets.ViewSet):
 
     @action(detail=False, permission_classes=[permissions.AllowAny])
     def get_music_by_iswc(self, request):
+        """
+        Intentionally used the filter queryset below instead of get, since the iswc field in the database is not unique
+        and avoiding a possible MultipleObjectReturned Error that my be thrown with the get queryset.
+        """
         try:
             iswc = self.request.query_params.get('iswc')
             music_list = list(Music.objects.filter(iswc=iswc, is_deleted=False).values())
